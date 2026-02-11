7b495a581c80c5154fc85917086ced14b7e459d1
Dev-scott <sadoscott25@gmail.com>
2026-02-11 23:55:10 +0100

    refactor

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>(
+      {
+        driver: ApolloDriver,
+        introspection: true,
+        fieldResolverEnhancers: ["guards"],
+        autoSchemaFile: join(process.cwd(), "src/graphql/schema.gql"),
+        buildSchemaOptions: {
+          numberScalarMode: 'integer',
+        }
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
