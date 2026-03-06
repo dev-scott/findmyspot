9614f867897ddc9022b61eb9f5bb999ac3f07ce6
Dev-scott <sadoscott25@gmail.com>
2026-03-06 13:38:46 +0100

    fix the prisma error

--- apps/api/src/util/prisma.ts
+++ apps/api/src/util/prisma.ts
@@ -1,8 +1,8 @@
 
 
-import { PrismaClient } from "../../generated/prisma/client";
-import { Injectable, OnModuleInit } from "@nestjs/common"
 
+import { Injectable, OnModuleInit } from "@nestjs/common"
+import { PrismaClient } from "../../generated/prisma/client"
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
